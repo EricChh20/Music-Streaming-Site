2fa065f6d1375737b2368de67f947e11b29a2d5e
Eric Chheang <et.chheang@gmail.com>
2020-02-02 14:48:22 -0500

    connected to mySQL user database

---
+++ Khmer-Song/includes/config.php
@@ -0,0 +1,11 @@
+<?php 
+    ob_start();
+    # connects database to local timezone
+    $timezone = date_default_timezone_set("America/New_York");
+    # connects to server with username, password, and databse name
+    $con = mysqli_connect("localhost","root","","slotify");
+    if(mysqli_connect_errno()) {
+        echo "Fail to connect: " . mysqli_connect_errno();
+    }
+
+?>
